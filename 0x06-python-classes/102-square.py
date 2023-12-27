6c6c0a57ff740908154d6cbfd8c88079bb4c5f56
NjeriKiarie <kcarolnjeri@gmail.com>
2023-12-27 11:36:48 -0800

    Compare

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -17,6 +17,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """sets the property"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
