ad5dbb8c1ebad45537d26a2c9bb5157aaacf81cd
NjeriKiarie <kcarolnjeri@gmail.com>
2024-01-01 06:29:25 -0800

    import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-"""program that imports the function def add(a, b):
-    from the file add_0.py and prints the result of
-    the addition 1 + 2 = 3."""
 from add_0 import add
 
 if __name__ == "__main__":
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""program that imports functions from the file calculator_1.py,
-does some Maths, and prints the result."""
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of variable a and b."""
-    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
