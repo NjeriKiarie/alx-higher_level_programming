a2bbbbb282e67a68447abb6bac54ff4c71299497
NjeriKiarie <kcarolnjeri@gmail.com>
2024-01-07 23:18:23 -0800

    Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines an empty class Rectangle
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    pass
