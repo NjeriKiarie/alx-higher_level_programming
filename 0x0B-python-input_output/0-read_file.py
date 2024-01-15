b1648dcc88466a0c317342394cbc71022449e88d
NjeriKiarie <kcarolnjeri@gmail.com>
2024-01-15 00:38:06 -0800

    Read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    """ function reads n prints lines of a text file (UTF8) prints it stdout """
+    """ function reads n prints lines of a text file"""
 
     with open(filename) as f:
         lines = f.read()
     print(lines, end="")
-
