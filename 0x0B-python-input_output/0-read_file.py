44b14dd6ef00422660c05764f72a6cfd36941a6a
NjeriKiarie <kcarolnjeri@gmail.com>
2024-02-02 06:08:33 -0800

    write

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 def read_file(filename=""):
     """ function reads n prints lines of a text file"""
 
-    with open(filename) as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         lines = f.read()
     print(lines, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 
-def read_lines(filename="", nb_lines=0):
+def write_file(filename="", text=""):
+    """
+    Write a function that writes a string to a text file (UTF8) and returns the number of characters written.
+    """
 
-        """ function reads n lines of a text file (UTF8) prints it stdout """
+    with open(filename, encoding='utf-8') as f:
+        return f.write(text)
 
-
-
-            with open(filename, encoding='utf-8') as f:
-
-                        lines = f.readlines()
-
-                                if nb_lines <= 0 or nb_lines >= len(lines):
-
-                                                [print(i, end='') for i in lines]
-
-                                                        else:
-
-                                                                        [print(lines[i], end='') for i in range(0, nb_lines)]
