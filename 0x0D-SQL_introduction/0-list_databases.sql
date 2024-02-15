91ab78f7b21d39fa5ec9603205b4b3cbdddaa2c9
NjeriKiarie <kcarolnjeri@gmail.com>
2024-02-15 10:49:08 -0800

    Change Class

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- All the databases
+-- A script that lists all databases of your MySQL server
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- All tables present
+-- A script that lists all the tables of a database in your MySQL server.
+SHOW TABLES;
