4eae2487d3804be4130c70d62338b3594ffc22d9
NjeriKiarie <kcarolnjeri@gmail.com>
2024-02-16 03:42:56 -0800

    Temperature

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,11 @@
+-- All records
+-- QQuerry all the records of the table second_table of the database
+SELECT
+        score,
+        name
+FROM
+        second_table
+WHERE
+        name IS NOT NULL
+ORDER BY
+        score DESC;
