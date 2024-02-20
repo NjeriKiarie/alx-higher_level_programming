ba3fe131ac76e67e195c141495eb3cc366d13e9a
NjeriKiarie <kcarolnjeri@gmail.com>
2024-02-20 05:13:08 -0800

    MYSQL

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- A a script that creates the MySQL server user user_0d_1.
+-- Query the user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
