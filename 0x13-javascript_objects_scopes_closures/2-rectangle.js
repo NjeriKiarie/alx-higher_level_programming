d69c8a8123c29629211fa40160f1e071b3cea34a
NjeriKiarie <kcarolnjeri@gmail.com>
2024-04-09 04:26:49 -0700

    Square

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -7,16 +7,13 @@
 // If w or h is equal to 0 or not a positive integer, create an empty object
 // Create an instance method called print() that prints the rectangle using the character x
 
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
-  print () {
-    for (let i = 0; i < this.height; i++;) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-};
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/urs/bin/node
+// A square class that defines a square and inherits from Rectangel of 4-rectangle.js
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+	constructor (size) {
+		super(size, size);
+	}
+}
+
+module.exports = Square;
+
